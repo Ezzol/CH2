abc06258948044f0831bdbb34e37f29e7f81d7ba
Ezzol <18054161@student.hhs.nl>
2021-03-18 16:04:19 +0100

    Almost there

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Ik probeerde hier de animatie onload uit te zetten of uit te stellen. Nu krijg je namlijk een animatie onload en dat is lelijk. 
-// window.onload = function(){
-//     setInterval(function(){
-//         document.body.className="delayAnimation";
-//     },1000);
-// };
-
-// setTimeout(function(){
-//     document.body.className="delayAnimation";
-// },4000);
-
-
 // Dit is voor een andere weergave overdag
 function clock(){
     var today = new Date();
@@ -18,47 +6,31 @@ function clock(){
         var minutes = today.getMinutes();
         var hour = today.getHours();
         var days = ['ZO', 'MA', 'DI', 'WO', 'DO', 'VR', 'ZA', 'ZO'];
-        // weekday[0] = "MA";
-        // weekday[1] = "DI";
-        // weekday[2] = "WO";
-        // weekday[3] = "DO";
-        // weekday[4] = "VR";
-        // weekday[5] = "ZA";
-        // weekday[6] = "ZO";
-
+        
         var rotation = (hour*15) + (minutes*0.25) + (seconds*(0.25/60));
-
         document.getElementById('globeWrapper').style.transform = 'rotate(' + rotation + 'deg)';
 
 
         // Dus vanaf 9 uur 'sochtends tot 9 uur 's avonds
         if (hour >= 7 && hour < 12) {
-            // document.body.style.backgroundImage = "url('img/stage1/o_lucht.jpg')";
-            // document.getElementById('tent').style.backgroundImage = "url('img/stage1/o_tent.png')";
             document.getElementById('tent').classList.remove('landscapeNight');
             document.getElementById('tent').classList.add('landscapeMorning');
             document.body.style.backgroundImage = "url('img/stage1/o_lucht.jpg')";
         }
 
             else if(hour >= 12 && hour < 17){
-                // document.body.style.backgroundImage = "url('img/stage2/m_lucht.jpg')";
-                // document.getElementById('tent').style.backgroundImage = "url('img/stage2/m_tent.png')";
                 document.getElementById('tent').classList.remove('landscapeMorning');
                 document.getElementById('tent').classList.add('landscapeNoon');
                 document.body.style.backgroundImage = "url('img/stage2/m_lucht.jpg')";
             }
             
             else if(hour >= 17 && hour < 20){
-                // document.body.style.backgroundImage = "url('img/stage3/a_lucht.jpg')";
-                // document.getElementById('tent').style.backgroundImage = "url('img/stage3/a_tent.png')";
                 document.getElementById('tent').classList.remove('landscapeNoon');
                 document.getElementById('tent').classList.add('landscapeEvening');
                 document.body.style.backgroundImage = "url('img/stage3/a_lucht.jpg')";
             }
 
             else if(hour >= 20 || hour < 7){
-                // document.body.style.backgroundImage = "url('img/stage4/n_lucht.jpg')";
-                // document.getElementById('tent').style.backgroundImage = "url('img/stage4/n_tent.png')";
                 document.getElementById('tent').classList.remove('landscapeEvening');
                 document.getElementById('tent').classList.add('landscapeNight');
                 document.body.style.backgroundImage = "url('img/stage4/n_lucht.jpg')";
@@ -66,20 +38,27 @@ function clock(){
             }
 
         // Als er minder dan 10 seconden is, dus 0 t/m 9, dan moet er een 0 voor de seconden
-        if (seconds < 10) {
+        if (seconds < 10){
             seconds = '0' + seconds;
         }
 
-    // document.getElementById('weekDay').innerHTML = today.getHours() + ':' + today.getMinutes() + ':' + seconds;
+        if (minutes < 10){
+            minutes = '0' + minutes;
+        }
+
+        if (hour == 24){
+            hour = '00';
+        }
+
+        else if (hour < 10){
+            hour = '0' + hour;
+        }
+
     document.getElementById('weekDay').innerHTML = days[today.getDay()];
     document.getElementById('hour').innerHTML = today.getHours();
     document.getElementById('minutes').innerHTML = today.getMinutes();
     document.getElementById('seconds').innerHTML = today.getSeconds();
-
-};
+}
 
 clock();
 setInterval(clock, 1000);
-
-
-
