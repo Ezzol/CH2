2e0fa0f4f276d6456c9bd9c2903332f39e91184b
Ezzol <18054161@student.hhs.nl>
2021-03-18 19:01:41 +0100

    Final :)
    Alle pseudocode staat er inmiddels bij :)

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 // Dit is voor een andere weergave overdag
 function clock(){
     var today = new Date();
-         // dit is alleen voor de seconden. Moet dus ook nog voor minuten en uren gebeuren. Dit MOET binnen de functie om elke seconde te verversen
+         // Met deze variabele haal ik de seconden, minuten en uren op. Dit MOET binnen de functie om elke seconde te verversen
         var seconds = today.getSeconds();
         var minutes = today.getMinutes();
         var hour = today.getHours();
+        // Deze variabele heeft een array waarin de dagen zitten. Te beginnen bij zondag, want zondag is dag 0
         var days = ['ZO', 'MA', 'DI', 'WO', 'DO', 'VR', 'ZA', 'ZO'];
         
+        // Deze variabele berekent hoeveel graden de zon en maan moeten bewegen per seconde, minuut en uur. Vervolgens wordt deze data in css toegevoegd en aangepast
         var rotation = (hour*15) + (minutes*0.25) + (seconds*(0.25/60));
         document.getElementById('globeWrapper').style.transform = 'rotate(' + rotation + 'deg)';
 
 
-        // Dus vanaf 9 uur 'sochtends tot 9 uur 's avonds
+        // Als het 7 uur of later is, maar voor 12 uur, dan ochtendlandschap en lucht laten zien.
         if (hour >= 7 && hour < 12) {
             document.getElementById('tent').classList.remove('landscapeNight');
             document.getElementById('tent').classList.add('landscapeMorning');
             document.body.style.backgroundImage = "url('img/stage1/o_lucht.jpg')";
         }
 
+            // Als het 12 uur of later is, maar voor 17 uur, dan middaglandschap en lucht laten zien.
             else if(hour >= 12 && hour < 17){
                 document.getElementById('tent').classList.remove('landscapeMorning');
                 document.getElementById('tent').classList.add('landscapeNoon');
                 document.body.style.backgroundImage = "url('img/stage2/m_lucht.jpg')";
             }
             
+            // Als het 17 uur of later is, maar voor 20 uur, dan avondlandschap en lucht laten zien.
             else if(hour >= 17 && hour < 20){
                 document.getElementById('tent').classList.remove('landscapeNoon');
                 document.getElementById('tent').classList.add('landscapeEvening');
                 document.body.style.backgroundImage = "url('img/stage3/a_lucht.jpg')";
             }
 
+            // Als het 20 uur of later is, maar voor 7 uur, dan nachtlandschap en lucht laten zien. Ook de astronautanimatie nu zichtbaar maken
             else if(hour >= 20 || hour < 7){
                 document.getElementById('tent').classList.remove('landscapeEvening');
                 document.getElementById('tent').classList.add('landscapeNight');
@@ -42,23 +47,28 @@ function clock(){
             seconds = '0' + seconds;
         }
 
+        // Als er minder dan 10 minuten is, dus 0 t/m 9, dan moet er een 0 voor de minuten
         if (minutes < 10){
             minutes = '0' + minutes;
         }
 
+        // Als het 24 uur is, weergeven als 00 uur.
         if (hour == 24){
             hour = '00';
         }
 
+        // Als er minder dan 10 uren is, dus 0 t/m 9, dan moet er een 0 voor de uren
         else if (hour < 10){
             hour = '0' + hour;
         }
 
+    // Haal de dag, het uur, de minuut en de seconde op en plaats deze in de juiste html tag
     document.getElementById('weekDay').innerHTML = days[today.getDay()];
-    document.getElementById('hour').innerHTML = today.getHours();
-    document.getElementById('minutes').innerHTML = today.getMinutes();
-    document.getElementById('seconds').innerHTML = today.getSeconds();
+    document.getElementById('hour').innerHTML = hour;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
 }
 
+// Run de klok en zet de interval van de klok op 1 seconde
 clock();
 setInterval(clock, 1000);
